8b15d6893051432f15d805c30de43db17bc657fb
SalvatierraFran <franco14_93@hotmail.com>
2016-03-16 11:10:17 -0300

    Ejemplo de uso de TryParse

---
+++ EjemplosClase1/Clase2Ejemplo3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clase2Ejemplo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
